4a44fcd4566da7870d58071254b3c8f415b0774c
markoter <markoterdev@outlook.com>
2022-08-31 18:46:34 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import EditorBox from "./Editor";
+import HeaderBox from "./Header";
 
 class App extends React.Component {
     render() {
         return (
-            <div id="app">
+            <div id="app-container">
+                <HeaderBox />
                 <p>this is just app</p>
                 <EditorBox />
             </div>
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeaderBox = () => {
+    return (
+        <div id="header">
+            <h1>Markdown Reviewer</h1>
+        </div>
+
+    )
+}
+
+export default HeaderBox;
