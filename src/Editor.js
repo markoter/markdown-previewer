2f3c23bd18b95c26e59ae0c8b1a9717401a88e4c
markoter <markoterdev@outlook.com>
2022-09-01 19:25:40 +0200

    working on site layout

--- src/Editor.js
+++ src/Editor.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const EditorBox = () => {
     return (
-        <div id="editorBox">
+        <div id="editorBox" className="textBox">
             <label for="editor">Editor</label>
+            <br />
             <textarea id="editor" name="editor" rows={4} cols="50" >
                 default textarea text
             </textarea>
--- src/Preview.js
+++ src/Preview.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const PreviewBox = () => {
     return (
-        <div id="previewBox">
+        <div id="previewBox" className="textBox">
             <label for="preview">Preview</label>
+            <br />
             <textarea id="preview" name="preview" rows={4} cols="50" >
                 default preview textarea text
             </textarea>
