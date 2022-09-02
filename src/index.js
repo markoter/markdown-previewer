67faa4a1822707be3158904e3a963cc71f8670c1
markoter <markoterdev@outlook.com>
2022-09-02 20:16:41 +0200

    start with redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import './index.css'
+import { createStore } from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
+
+const textReducer = (state = "", action) => {
+    switch (action.type) {
+        case "ADDTEXT":
+            return state;
+        case "REMOVETEXT":
+            return state
+    }
+}
+
+let store = createStore(textReducer);
