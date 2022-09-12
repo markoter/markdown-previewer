01e112f35c3ba76e3d4c2b46bf6f500c12e550ed
markoter <markoterdev@outlook.com>
2022-09-13 00:11:26 +0200

    abort redux for now

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import './index.css'
-import { createStore } from "redux";
+//import { createStore } from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
 
-const textReducer = (state = "", action) => {
-    switch (action.type) {
-        case "ADDTEXT":
-            return state;
-        case "REMOVETEXT":
-            return state
-    }
-}
+// const textReducer = (state = "", action) => {
+//     switch (action.type) {
+//         case "ADDTEXT":
+//             return state;
+//         case "REMOVETEXT":
+//             return state
+//     }
+// }
 
-let store = createStore(textReducer);
+// let store = createStore(textReducer);
