dd027a662f7c43391166005ee8025536cf1455e3
markoter <markoterdev@outlook.com>
2022-09-13 00:46:00 +0200

    working on state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import HeaderBox from "./Header";
 import PreviewBox from "./Preview";
 
 class App extends React.Component {
+    state = {
+        text: ""
+    }
+
+    handleTextChange = event => {
+        
+    }
     render() {
+        const { text } = this.state
         return (
             <div id="app-container">
                 <HeaderBox />
                 <div id="content">
                     <EditorBox />
-                    <PreviewBox />
+                    <PreviewBox text={text} writeText={this.writeText} />
                 </div>
 
             </div>
